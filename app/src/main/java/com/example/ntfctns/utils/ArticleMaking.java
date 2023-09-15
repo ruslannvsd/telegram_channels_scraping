3957780fc6fc0495bbeda0275adbb96901ecd9fc
Admin <ruslan.novosad.sf@gmail.com>
2023-09-15 18:56:06 +0100

    imprvmnts, bugs fixed

---
+++ app/src/main/java/com/example/ntfctns/utils/ArticleMaking.java
@@ -0,0 +1,53 @@
+package com.example.ntfctns.utils;
+
+import static com.example.ntfctns.consts.Cons.ART_META;
+import static com.example.ntfctns.consts.Cons.DATETIME;
+import static com.example.ntfctns.consts.Cons.D_TIME;
+import static com.example.ntfctns.consts.Cons.LINK;
+import static com.example.ntfctns.consts.Cons.SECTION;
+
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.example.ntfctns.classes.Article;
+
+import org.jsoup.nodes.Element;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleMaking {
+    @Nullable
+    public Article makeArticle(Element el, String body, String keyW, int hours) {
+        if (hours != 0) {
+            long now = System.currentTimeMillis();
+            long hoursMilli = (long) hours * 60 * 60 * 1000;
+            long threshold = now - hoursMilli;
+            String articleTime = el.select("span." + ART_META + DATETIME).attr(D_TIME);
+            Log.i("custom Time String initial : ", articleTime);
+            long millis = TimeConverter.convertToMillis(articleTime);
+            Log.i("custom Time Str -> long : ", String.valueOf(millis));
+            Log.i("custom Time long -> Str : ", TimeConverter.convertToReadableTime(millis));
+
+            if (millis >= threshold) {
+                String imgLink = WordFuncs.getLink(el);
+                Element linkElement = el.select("span." + ART_META + " a." + SECTION).first();
+                String art_link = linkElement.attr(LINK);
+                List<String> keywords = new ArrayList<>();
+                keywords.add(keyW);
+                return new Article(imgLink, art_link, body, millis, keywords);
+            }
+        } else {
+            String articleTime = el.select("span." + ART_META + DATETIME).attr(D_TIME);
+            long millis = TimeConverter.convertToMillis(articleTime);
+            String imgLink = WordFuncs.getLink(el);
+            Element linkElement = el.select("span." + ART_META + " a." + SECTION).first();
+            String art_link = linkElement.attr(LINK);
+            List<String> keywords = new ArrayList<>();
+            keywords.add(keyW);
+            return new Article(imgLink, art_link, body, millis, keywords);
+        }
+        return null;
+    }
+}
